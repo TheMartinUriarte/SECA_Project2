4f469bc4b404ce101a275354532f9f5b920eb737
TheMartinUriarte <themartinuriarte.programmer@gmail.com>
2018-03-25 17:07:47 -0700

    [NEW] Created Users database table

---
+++ users-api/src/main/resources/db/migration/V1__Create_users_table.sql
@@ -0,0 +1,13 @@
+create table USERS (
+    ID serial,
+    UUID varchar(255),
+    USER_NAME varchar(255),
+    LOWER_USER_NAME varchar(255),
+    FIRST_NAME varchar(255),
+    LOWER_FIRST_NAME varchar(255),
+    LAST_NAME varchar(255),
+    LOWER_LAST_NAME varchar(255),
+    EMAIL_ADDRESS varchar(255),
+    LOWER_EMAIL_ADDRESS varchar(255),
+    DATE_CREATED varchar(255)
+)
